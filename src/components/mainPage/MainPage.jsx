11341fd5fc0cac18e1c4c2b9ad84581f84a51320
sayasha <2two2two8eight@mail.ru>
2024-01-22 01:09:41 +0300

    added ability to delete a file

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,13 +1,20 @@
 import './mainPage.css'
 import Header from './header/Header'
 import FileItem from './fileItem/FileItem'
+import filesData from './file.data'
+import { useState } from 'react'
 
 function FieldItems(){
+    const [files, setFile] = useState(filesData)
     return(
         <>
         <Header />
         <main>
-            <FileItem />
+            {files.length ? (
+                files.map(file => <FileItem key={file.id} file={file} setFile={setFile}/>)
+            ) : (
+                <p>There are no files</p>
+            )}
         </main>
         </>
     )
