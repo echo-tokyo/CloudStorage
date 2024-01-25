b3f55f254bd5154bdccdc8a19f44e94b0f725cd0
Danil-114195722 <ejyou.user@gmail.com>
2024-01-25 20:53:11 +0300

    Added all including urlpatterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/user/', include('users.urls')),
+    # path('api/user-profile/', include('user_profile.urls')),
+    # path('api/storage/', include('storage_api.urls')),
+    # path('api/reviews/', include('reviews.urls')),
 ]
