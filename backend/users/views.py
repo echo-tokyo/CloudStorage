a2741c3608678c389bc998b0a642087dced167d6
Danil-114195722 <ejyou.user@gmail.com>
2024-01-25 20:53:58 +0300

    Added serializers and views for login and reg

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
+
+
+class UserRegistrationAPIView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserRegistrationSerializer
+
+    def post(self, request: Request):
+        user = request.data
+
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class UserLoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserLoginSerializer
+
+    def post(self, request: Request):
+        user = request.data
+
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        print(serializer.data)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
