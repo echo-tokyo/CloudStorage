70edc094b35ba4742e48421f037035c63a669bb6
Danil-114195722 <ejyou.user@gmail.com>
2024-01-26 01:29:57 +0300

    Fixed bugs with crushing backend container

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,4 +5,4 @@ cd /app/backend
 #python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
 
-gunicorn backend.wsgi --workers=3 --bind 0.0.0.0:8080
+gunicorn --workers=3 --bind 0.0.0.0:8080 backend.wsgi
