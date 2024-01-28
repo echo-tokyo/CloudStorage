0c34f7ec9db3a40e8207a60bcd76ce4309ad7a33
Danil-114195722 <ejyou.user@gmail.com>
2024-01-28 03:41:44 +0300

    Fixed UserProfileSerializer: set working static link for profile photo

--- backend/user_profile/serializers.py
+++ backend/user_profile/serializers.py
@@ -1,15 +1,22 @@
+from django.conf import settings
 from rest_framework import serializers
 
 from .models import Profile
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    id = serializers.SerializerMethodField()
+    id = serializers.IntegerField()
 
     class Meta:
         model = Profile
         fields = ('id', 'nickname', 'photo')
 
-    def get_id(self, obj: Profile):
-        """Return user_id as id"""
-        return obj.user_id
+    def to_representation(self, instance):
+        print(instance.photo)
+
+        representation = {
+            "id": instance.user.id,
+            "nickname": instance.nickname,
+            "photo": f'http://{settings.IP_OR_DNS_SERVER}/static/{instance.photo}'
+        }
+        return representation
