faaa12020583fac65be5b7618c97bacc43585b5b
Danil-114195722 <ejyou.user@gmail.com>
2024-02-06 00:54:25 +0300

    Remove user_profile app; merge it with users

--- backend/user_profile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import GetUserProfileAPIView, EditUserProfileAPIView
-
-
-urlpatterns = [
-    path('get/', GetUserProfileAPIView.as_view()),
-    path('edit-photo/', EditUserProfileAPIView.as_view()),
-]
