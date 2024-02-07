c91871e99b95381657ef01b6ee48087ea3d68cf6
sayasha <2two2two8eight@mail.ru>
2024-02-07 22:52:54 +0300

    nothing spectial

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Cloud Storage
 
-## Backend & forntend
-
 #### Status of deployment:
 [![Backend-CD](https://github.com/echo-tokyo/CloudStorage/actions/workflows/backend_deploy.yml/badge.svg?branch=backend)](https://github.com/echo-tokyo/CloudStorage/actions/workflows/backend_deploy.yml)
 
--- src/components/mainPage/header/Header.jsx
+++ src/components/mainPage/header/Header.jsx
@@ -8,16 +8,14 @@ function Header({changeTheme, modalOpen, profileClick, profilePhoto, setFile, fo
         const token = localStorage.getItem('token')
 
         axios.post('http://79.137.204.172/api/user/logout/', token, {headers: {'Authorization': `Bearer ${token}`}})
-        .then(response => {
-            console.log(response.data)
+        .then(() => {
+            navigate('/login')
+            localStorage.removeItem('token')
+            localStorage.removeItem('rootDir')
         })
         .catch(error => {
             console.error('Произошла ошибка при выходе ', error)
         })
-        
-        navigate('/login')
-        localStorage.removeItem('token')
-        localStorage.removeItem('rootDir')
     }
 
     const filesPush = (e) => {
