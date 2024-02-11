87bd9db1563435d10e53e17be66f5ca514d5b5c7
Danil-114195722 <ejyou.user@gmail.com>
2024-02-11 17:06:48 +0300

    Removed /admin path from app urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/user/', include('users.urls')),
     path('api/storage/', include('storage_api.urls')),
     # path('api/reviews/', include('reviews.urls')),
